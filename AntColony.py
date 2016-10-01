829b69f827cd946af41e511e73e0d9685d595dfd
Mohammad H. Nazemi <nazemimohammadh@gmail.com>
2016-10-01 23:13:04 +0330

    Fixing FindAPath Func
    some major bugs fixed.

--- AntColony.py
+++ AntColony.py
@@ -11,18 +11,30 @@ class Ant:
     def FindAPath(self,distances,pheromones):
         currentCityIndex=random.randint(0,len(distances))
 
+        path=[]
+
         lenDistances=len(distances[currentCityIndex])
         probabilityDenominator=0
-        for j in pheromones[currentCityIndex]:
-            probabilityDenominator = probabilityDenominator + (pheromones[currentCityIndex][j]*((1/distances[currentCityIndex][j])**2))
+        for j in range(0,len(pheromones[currentCityIndex])):
+            if j!=currentCityIndex:
+                probabilityDenominator = probabilityDenominator + (pheromones[currentCityIndex][j]*((1/distances[currentCityIndex][j])**2))
 
-        for i in lenDistances-1:
-            del distances[currentCityIndex][currentCityIndex]
+        selectedIndex=[0]*len(distances[currentCityIndex])
+        for i in range(0,lenDistances-1):
+            selectedIndex[currentCityIndex]=1
             randomIndex=random.uniform(0,probabilityDenominator)
             cumulativeProbability=0
-            for k in distances[currentCityIndex]:
+            for k in range(0,len(distances[currentCityIndex])):
+
+                if selectedIndex[k]==1:
+                    continue
                 cumulativeProbability= cumulativeProbability + (pheromones[currentCityIndex][k]*((1/distances[currentCityIndex][k])**2))
                 if randomIndex < cumulativeProbability:
+                    path.append([currentCityIndex,k])
+
+                    probabilityDenominator=probabilityDenominator-(pheromones[currentCityIndex][k]*((1/distances[currentCityIndex][k])**2))
+                    print(k)
+
                     currentCityIndex = k
                     break
 
